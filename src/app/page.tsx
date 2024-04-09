3aa731d77122924cd32f478fc56caf03775568c1
Owais Athar <owaisathar2882@gmail.com>
2024-04-10 01:05:46 +0530

    add main logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,202 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+import Confetti from "react-confetti";
+
+const Player = {
+  None: null,
+  One: "X",
+  Two: "O",
+  Three: "Z",
+};
+
+function App() {
+  const [board, setBoard] = useState(
+    Array(5)
+      .fill(0)
+      .map(() => Array(5).fill(Player.None))
+  );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [winner, setWinner] = useState("");
+  const [windowWidth, setWindowWidth] = useState(0);
+  const [windowHeight, setWindowHeight] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.outerHeight);
+  }, []);
+
+  const [currentPlayer, setCurrentPlayer] = useState(Player.One);
+
+  const confettiConfig = {
+    angle: 90,
+    spread: 360,
+    startVelocity: 30,
+    elementCount: 70,
+    dragFriction: 0.12,
+    duration: 3000,
+    stagger: 3,
+    width: "10px",
+    height: "10px",
+    perspective: "500px",
+    colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"],
+  };
+
+  function handleClick(x: number, y: number) {
+    if (board[x][y] !== Player.None) return;
+    // Create a copy of the board and update it
+    const newBoard = board.map((row) => [...row]);
+    newBoard[x][y] = currentPlayer;
+    // Check the winner using the updated board
+    const winner = checkWinner(newBoard);
+    if (winner) {
+      setWinner(winner);
+      setIsModalOpen(true);
+    }
+    // Update the state
+    setBoard(newBoard);
+    setCurrentPlayer((prev) =>
+      prev === Player.One
+        ? Player.Two
+        : prev === Player.Two
+        ? Player.Three
+        : Player.One
+    );
+  }
+
+  function resetGame() {
+    setBoard(
+      Array(5)
+        .fill(0)
+        .map(() => Array(5).fill(Player.None))
+    );
+    setCurrentPlayer(Player.One);
+    setWinner("");
+  }
+
+  function checkWinner(board: Array<Array<string>>) {
+    // Check rows
+    for (let i = 0; i < 5; i++) {
+      if (
+        board[i][0] &&
+        board[i][0] === board[i][1] &&
+        board[i][1] === board[i][2] &&
+        board[i][2] === board[i][3] &&
+        board[i][3] === board[i][4]
+      ) {
+        return board[i][0];
+      }
+    }
+
+    // Check columns
+    for (let i = 0; i < 5; i++) {
+      if (
+        board[0][i] &&
+        board[0][i] === board[1][i] &&
+        board[1][i] === board[2][i] &&
+        board[2][i] === board[3][i] &&
+        board[3][i] === board[4][i]
+      ) {
+        return board[0][i];
+      }
+    }
+
+    // Check diagonals
+    if (
+      board[0][0] &&
+      board[0][0] === board[1][1] &&
+      board[1][1] === board[2][2] &&
+      board[2][2] === board[3][3] &&
+      board[3][3] === board[4][4]
+    ) {
+      return board[0][0];
+    }
+    if (
+      board[0][4] &&
+      board[0][4] === board[1][3] &&
+      board[1][3] === board[2][2] &&
+      board[2][2] === board[3][1] &&
+      board[3][1] === board[4][0]
+    ) {
+      return board[0][4];
+    }
+
+    return null;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      {winner && <Confetti width={windowWidth} height={windowHeight} />}
+      <div className="grid grid-cols-5 gap-4">
+        {board.map((row: Array<string>, x: number) =>
+          row.map((cell: string, y: number) => (
+            <button
+              key={`${x}-${y}`}
+              className={`w-14 h-14 bg-white border-2 border-gray-200 rounded-md flex items-center justify-center text-2xl font-bold 
+              ${
+                cell === Player.One
+                  ? "text-red-500"
+                  : cell === Player.Two
+                  ? "text-blue-500"
+                  : "text-green-500"
+              }`}
+              onClick={() => handleClick(x, y)}
+            >
+              {cell}
+            </button>
+          ))
+        )}
+        {isModalOpen && (
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+              <div
+                className="fixed inset-0 transition-opacity"
+                aria-hidden="true"
+              >
+                <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+              </div>
+              <span
+                className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                aria-hidden="true"
+              >
+                &#8203;
+              </span>
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <h3
+                        className="text-lg leading-6 font-medium text-gray-900"
+                        id="modal-title"
+                      >
+                        Player {winner} Wins!
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={() => {
+                      setIsModalOpen(false);
+                      resetGame();
+                    }}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="mt-4 text-2xl">
+        Current Player: <strong>{currentPlayer}</strong>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
+
+export default App;
